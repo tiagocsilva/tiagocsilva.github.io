630f4330c9229a0654c60c319b2bb37ecf2ce9a6
Tiago Silva <ltiago90@gmail.com>
2016-07-17 16:23:48 -0300

    New Scripts

--- js/script.js
+++ js/script.js
@@ -1,29 +1,79 @@
 $(document).ready(function() {
-	var hg = $(window).height();
-	//hg -= parseInt($("aside").css("padding-top"));
-	//hg -= parseInt($("aside").css("padding-bottom"));
-
 	
-
+	//PEGA O TÍTULO DA PÁGINA ATUAL
+	var currentPage = document.title;
+	
+	//PEGA ALTURA E LARGURA DA PÁGINA
+	var hg = $(window).height();
 	var wd = $(window).width();
 
+	//AJUSTA O TAMANHO DA PÁGINA NOS DIFERENTES TAMANHPS DE TELA
 	if(wd > 760) {
-		
 		wd -= $("aside").width();
-		//wd -= wd * 0.1;
 		$("section").width(wd);	
-
-	}
-	else
-	{
-		hg -= 100;
 	}
 
 	$("aside").height(hg);
-	$("section").height(hg);
+	
+	if(currentPage != "Portugol") {
+		if(wd > 760) {
+			$("section").height(hg);
+		}
+		else
+		{
+			$("section").height(hg - 100);
+		}
+		$("aside").height(hg);
+	}
+
+	//VERIFICA SE A PÁGINA É A PORTUGOL E CARREGA A LISTA DE ALGORITMOS
+	if(currentPage == "Portugol") {
+		$("#portugol").load("alg.html");
+	}
+
+	//CONTROLE DE ABERTURA DO MENU
+	$("#menuOpen").click(function() {
+		$("section").css("transition","height 1.2s");
+		$("section").height(hg);
+		$("aside > *").hide();
+		$("#menuOpen").hide();
+		$("aside").width(0);
+		$("aside").show();
+		$("aside").width(300);
+		$("aside > *").show(500);
+	});
+
+	//CONTROLE DE FECHAMENTO DO MENU
+	$("#menuClose").click(function() {
+		$("section").css("transition","none");
+		$("section").height(hg - 100);
+		$("aside > *").hide(600);
+		$("aside").hide(600);
+		$("#menuOpen").show(600);		
+	});
+
+	//DEFINE A PÁGINA ATUAL NO MENU
+	switch(currentPage) {
+		case "Tiago Silva":
+			$("nav button:eq(0)").css("border-color","#2790B0");
+			break;
+
+		case "WebSites":
+			$("nav button:eq(1)").css("border-color","#2790B0");
+			break;
+
+		case "C/C++":
+			$("nav button:eq(2)").css("border-color","#2790B0");
+			break;
+
+		case "Portugol":
+			$("nav button:eq(3)").css("border-color","#2790B0");
+			break;
+	}
 
 });
 
+//CONTROLE DO SLID DOS TÍTULOS DA PÁGINA INICIAL
 var i = 1;
 
 function contentFadeOut() {
@@ -53,4 +103,14 @@ function contentFadeOut() {
 		break;
 
 	}
+}
+
+//CONTROLE DA MOSTRA DE ALGORITMOS
+function showAlgorithm(n) {
+	$("#portugol").load("alg/" + n + ".html");
+}
+
+//VOLTAR A LISTA DE ALGORITMOS
+function showList() {
+	$("#portugol").load("alg.html");
 }
